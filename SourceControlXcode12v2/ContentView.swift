52495de6039a97e3715a5ed53b742dc32d6682e1
Christian Moysa <cmoysa@gmail.com>
2021-04-25 11:48:31 -0700

    Initial Commit

---
+++ SourceControlXcode12v2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SourceControlXcode12v2
+//
+//  Created by Christian Moysa on 2021-04-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SourceControlXcode12v2/SourceControlXcode12v2App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXcode12v2App.swift
+//  SourceControlXcode12v2
+//
+//  Created by Christian Moysa on 2021-04-25.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXcode12v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
